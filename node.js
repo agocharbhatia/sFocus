b0dc331de896164cf7ac409ba69c9549d69589db
Agochar Bhatia <abcraze11@gmail.com>
2019-06-01 12:26:45 -0400

    Added and Tested Smooth Scroll for Future Use

--- node.js
+++ node.js
@@ -6,5 +6,10 @@ var typed = new Typed('#typed', {
   stringsElement: '#typed-strings',
   typeSpeed: 40,
   backSpeed: 20,
-  backDelay: 500
+  backDelay: 1000
+});
+
+var scroll = new SmoothScroll('a[href*="#"]', {
+	speed: 500,
+	speedAsDuration: true
 });
